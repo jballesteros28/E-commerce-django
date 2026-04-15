f37767bc73329fd38ef861418b3aca17e23f5e67
juan david <juandavidballesterosbayona@gmail.com>
2026-04-15 11:30:30 -0300

    static_root

--- tienda_ecommerce/settings.py
+++ tienda_ecommerce/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*.onrender.com",
+    "https://*e-commerce-django-gbcs.onrender.com",
 ]
 
 # Application definition
@@ -134,7 +134,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATIC_ROOT = BASE_DIR / 'staticfiles'
     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 STATICFILES_DIRS = [BASE_DIR / 'static']
