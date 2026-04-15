f00822c76830a1ef6e99db371cb52a312cc30686
juan david <juandavidballesterosbayona@gmail.com>
2026-04-15 11:46:20 -0300

    csrf

--- tienda_ecommerce/settings.py
+++ tienda_ecommerce/settings.py
@@ -34,7 +34,7 @@ ALLOWED_HOSTS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*e-commerce-django-gbcs.onrender.com",
+    "https://e-commerce-django-gbcs.onrender.com",
 ]
 
 # Application definition
